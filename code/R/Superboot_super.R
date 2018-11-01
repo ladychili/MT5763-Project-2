9a6accaf6d2d8020c12d3c86ee636e7f24a6f0fc
alexis2113 <43744699+alexis2113@users.noreply.github.com>
2018-11-01 23:01:34 +0000

    Update Superboot_super.R

--- code/R/Superboot_super.R
+++ code/R/Superboot_super.R
@@ -1,4 +1,9 @@
 
+
+
+
+
+
 #######A super fast version#####
 #  darwbacks: 1. only accept matrix 
 #             2. singularity problems
@@ -25,21 +30,74 @@ lmBoot.v1 <- function(nboots,x,y){
 
 
 ##########################Super robust and super fast###########
-lmBoot.v2 <- function(nboots,x,y){
-     #data.frame or matrix are acceptable
-     #must seperate xs and ys
-     clusterExport(cl,list('x','y','nboots'),envir =.GlobalEnv)
-     x <- model.matrix(y~x,data=data.frame(x,y))
-     #using qr.rank to get rid of correlated variables
-     x <- x[,qr(x)$pivot[1:qr(x)$rank]]
-     y <- as.matrix(inputdata['y'])
-     i=1:nboots
-    allcoefs <- parLapply(cl,i,fun = function(i){
-          set.seed(i)
-          indexs <- sample(1:nrow(y),nrow(y),replace = TRUE)
-          #qr.solve()may be faster
-          ixx <- crossprod(x[indexs,],x[indexs,])
-          list(solve(ixx)%*%crossprod(x[indexs,],y[indexs,]))
-          })
+
+mycl <- makeSOCKcluster(c('localhost','localhost'))
+
+lmBoot.super <- function(nboots=1000,inputdata,formula,cl,seeds){
+  #
+  # 
+  # data.frame only
+  # convert all categorical 
+  # variable into appropriate class first
+  # formula must be in the form of y..~x+....
+  # 
+  # nboots: number of bootstrap,default to 1000
+  # inputdata: dataframe with all the variables necessary in formula
+  # CL: The cluster
+  # 
+  # step.1 
+  # 
+  # transfrom data.frame into matrixs
+  # all variables with class of factors will
+  # be converted to dummy variables and
+  # intercepts are automactically included
+  # 
+  x <- model.matrix(formula,data=inputdata)
+  y <- as.character(formula)[2]
+  y <- as.matrix(inputdata[y])
+  #
+  #Step 2: 
+  # using qr.rank to get rid of correlated variables
+  # To ensure that t(X)*X is invertible
+  # Detail explanation:
+  # https://en.wikipedia.org/wiki/Invertible_matrix
+  # Correlated variables are omitted
+  # 
+  x <- x[,qr(x)$pivot[1:qr(x)$rank]]
+  #
+  #Step 3:
+  #
+  #Parlappy to perform parallel computation
+  #two sets of seeds(
+  #
+  set.seed(seeds)
+  i <- sample(1:nboots,nboots,replace = FALSE)
+  allcoefs <-parLapply(
+    cl,i,fun=function(i=i){
+    set.seed(i)
+    #
+    # sample index instead of the whole matrix
+    # is faster
+    #
+    indexs <- 
+      sample(1:nrow(y),nrow(y),replace = TRUE)
+    #
+    #calculating t(X)X
+    #
+    ixx <- crossprod(x[indexs,],x[indexs,])
+    #
+    # Solving the linear equation system
+    # Using QR decomposition
+    # See for proofs and details:
+    # https://en.wikipedia.org/wiki/Linear_least_squares
+    # In fact, this is excatly what lm() is doing inside.
+    # except that lm() do a lot more than just computating
+    # coefficients
+    #
+    list(solve(ixx)%*%crossprod(x[indexs,],y[indexs,]))
+  })
 }
 
+
+
+tests <- lmBoot.super(nboots=1000,inputdata=babiesDf,formula=as.formula(wt~dwt+number),cl=mycl,seeds=1)
