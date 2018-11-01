6701417713fb4cd8e0df80818581e4c6fdb5b975
alexis2113 <43744699+alexis2113@users.noreply.github.com>
2018-11-01 23:30:08 +0000

    Update Superboot_super.R

--- code/R/Superboot_super.R
+++ code/R/Superboot_super.R
@@ -1,27 +1,4 @@
 
-#######A super fast version#####
-#  darwbacks: 1. only accept matrix 
-#             2. singularity problems
-#             3. can only get coefficients
-#### faster than boot though ~~~~~
-
-
-lmBoot.v1 <- function(nboots,x,y){
-     cl <- makePSOCKcluster(c("localhost","localhost"))
-     clusterExport(cl,list =list('x','y','nboots'),envir =.GlobalEnv)
-  
-    index <- matrix(sample(
-      1:nrow(y),nrow(y)*nboots,replace = TRUE
-      ),nrow=nboots,ncol=nrow(y),byrow = TRUE)
-    
-    allcoefs <- parRapply(cl,index,fun = function(indexs){
-          ixx <- crossprod(x[indexs,],x[indexs,])
-          if(det(ixx)!=0){list(
-            solve(ixx)%*%crossprod(x[indexs,],y[indexs,]))
-          }
-        })
-      }
-
 ######################## The final version ##############################
 
 
@@ -96,3 +73,28 @@ lmBoot.super <- function(nboots=1000,inputdata,formula,cl,seeds){
 
 
 tests <- lmBoot.super(nboots=1000,inputdata=babiesDf,formula=as.formula(wt~dwt+number),cl=mycl,seeds=1)
+
+
+
+#######  Previous Version #####
+#  darwbacks: 1. only accept matrix 
+#             2. singularity problems
+#             3. can only get coefficients
+#### faster than boot though ~~~~~
+
+
+lmBoot.v1 <- function(nboots,x,y){
+     cl <- makePSOCKcluster(c("localhost","localhost"))
+     clusterExport(cl,list =list('x','y','nboots'),envir =.GlobalEnv)
+  
+    index <- matrix(sample(
+      1:nrow(y),nrow(y)*nboots,replace = TRUE
+      ),nrow=nboots,ncol=nrow(y),byrow = TRUE)
+    
+    allcoefs <- parRapply(cl,index,fun = function(indexs){
+          ixx <- crossprod(x[indexs,],x[indexs,])
+          if(det(ixx)!=0){list(
+            solve(ixx)%*%crossprod(x[indexs,],y[indexs,]))
+          }
+        })
+      }
